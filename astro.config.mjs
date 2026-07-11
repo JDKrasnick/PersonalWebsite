a078d38cca467e4c6ec46e49826a5eab49fc2df3
JD <jdk342@cornell.edu>
2026-07-11 03:03:06 -0400

    Improve portfolio SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://www.jdkrasnick.com',
   output: 'static',
   adapter: vercel(),
 });
